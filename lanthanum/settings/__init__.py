0573f501b76743cf252e906888a22f030aa79eed
William Brown <william.e.brown@adelaide.edu.au>
2011-12-14 12:07:18 +1030

    improved some settings stuff

--- lanthanum/settings/__init__.py
+++ lanthanum/settings/__init__.py
@@ -11,3 +11,10 @@ try:
     exec 'from %s import *' % (platform.node().split(".")[0].lower())
 except ImportError:
     pass
+except SyntaxError:
+	pass
+
+if SERVER_CLASS.lower() == "dev" or SERVER_CLASS.lower() == "test":
+	DEBUG=True
+elif SERVER_CLASS.lower() == "prd":
+	DEBUG=False
